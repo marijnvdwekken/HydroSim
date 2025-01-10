a60b07aff8f12d4396a23f38e1f4ea9797c88bba
Vincent Hasper <125301365+haspervincent@users.noreply.github.com>
2025-01-10 15:38:55 +0100

    Update test_servers.py

--- epanet/app/test_servers.py
+++ epanet/app/test_servers.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-import time
 import sys
 import threading
+import time
 
 from pyModbusTCP.server import ModbusServer
+
 
 def start_server(host, port):
     try:
         server = ModbusServer(host=host, port=port, no_block=True)
-        server.data_bank.set_holding_registers(0, [0]*100)
-        
+        server.data_bank.set_holding_registers(0, [0] * 100)
+
         server.start()
 
         if server.is_run:
@@ -18,41 +19,43 @@ def start_server(host, port):
 
             while True:
                 hr = server.data_bank.get_holding_registers(0, 100)
-                print(f"\nhr ({port}): {hr}")
+                print(f"\nholding registers ({port}): {hr}")
                 time.sleep(3)
     except Exception as e:
         print(f"ERROR on server {port}: {e}")
         sys.exit(1)
     finally:
-        if 'server' in locals():
+        if "server" in locals():
             server.stop()
+
 
 def main():
     try:
         servers_config = [
-            ('127.0.0.1', 502),
-            ('127.0.0.1', 503),
-            ('127.0.0.1', 504),
-            ('127.0.0.1', 505),
-            ('127.0.0.1', 506),
+            ("127.0.0.1", 502),
+            ("127.0.0.1", 503),
+            ("127.0.0.1", 504),
+            ("127.0.0.1", 505),
+            ("127.0.0.1", 506),
         ]
 
         threads = []
 
         for host, port in servers_config:
             thread = threading.Thread(target=start_server, args=(host, port))
-            thread.daemon = True # ensures threads exit when the main program exits.
+            thread.daemon = True  # ensures threads exit when the main program exits.
             threads.append(thread)
             thread.start()
-        
+
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         print(">--- Program interrupted by user ---")
-        sys.exit(0) # clean exit confirmed by user action.
+        sys.exit(0)  # clean exit confirmed by user action.
     except Exception as e:
         print(f"ERROR in main: {e}")
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
