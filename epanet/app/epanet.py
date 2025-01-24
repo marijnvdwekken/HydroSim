8a8ae7f1f8f50b46b31b7abb72010169b3da12bf
Vincent Hasper <125301365+haspervincent@users.noreply.github.com>
2025-01-24 23:43:38 +0100

    Update epanet.py

--- epanet/app/epanet.py
+++ epanet/app/epanet.py
@@ -31,20 +31,17 @@ def get_zones(en: epanet) -> set[str]:
 
 def setup_clients(zones: set) -> dict[str, ModbusTcpClient]:
     try:
-        clients: dict[str, ModbusTcpClient] = {
-            zone: ModbusTcpClient(host=f'plc-{zone}', port=502)
-            for zone in zones
-        }
-        ### TEST (CODE FOR LOCAL TESTING)
         # clients: dict[str, ModbusTcpClient] = {
-        #     zone: ModbusTcpClient(host="127.0.0.1", port=502 + i)
-        #     for i, zone in enumerate(zones)
+        #     zone: ModbusTcpClient(host=f'plc-{zone}', port=502)
+        #     for zone in zones
         # }
-        ### END
-        for zone, client in clients.items():
+        clients: dict[str, ModbusTcpClient] = {
+            zone: ModbusTcpClient(host="127.0.0.1", port=502 + i) 
+            for i, zone in enumerate(zones)
+        }
+        for _, client in clients.items():
             while not client.connect():
                 time.sleep(1)
-                print(f"brahim, {client} in {zone} werkt niet.")
         return clients
     except Exception as e:
         print(f"ERROR in setup_clients: {e}")
@@ -54,9 +51,7 @@ def setup_clients(zones: set) -> dict[str, ModbusTcpClient]:
 def setup_epanet(inp_file: str) -> epanet:
     try:
         en: epanet = epanet(inp_file)
-        en.setTimeSimulationDuration(
-            10
-        )  # initial setup; duration will be set to infinite in main function.
+        en.setTimeSimulationDuration(10)  # initial setup; duration will be set to infinite in main function.
         en.setTimeHydraulicStep(1)
         return en
     except Exception as e:
@@ -64,77 +59,67 @@ def setup_epanet(inp_file: str) -> epanet:
         sys.exit(1)
 
 
-# def get_controls(clients: dict[str, ModbusTcpClient], en: epanet) -> dict:
-#     try:
-#         controls: dict = {}
+def get_controls(clients: dict[str, ModbusTcpClient], en: epanet) -> dict:
+    try:
+        controls: dict = {}
 
-#         for name_id in en.getNodeNameID() + en.getLinkNameID():
-#             if "-" not in name_id:
-#                 continue
-#             zone, element = name_id.split("-", 1)
-#             controls.setdefault(zone, {})
+        for name_id in en.getNodeNameID() + en.getLinkNameID():
+            if "-" not in name_id:
+                continue
+            zone, element = name_id.split("-", 1)
+            controls.setdefault(zone, {})
 
-#             if name_id in en.getLinkNameID():
-#                 link_index: int = en.getLinkIndex(name_id)
+            if name_id in en.getLinkNameID():
+                link_index: int = en.getLinkIndex(name_id)
 
-#                 match en.getLinkType(link_index):
-#                     case "PIPE":
-#                         pass
-#                     case "PUMP":
-#                         controls[zone].setdefault(element, {})
-#                         controls[zone][element]["speed"] = None
-#                     case _:
-#                         controls[zone].setdefault(element, {})
-#                         controls[zone][element]["setting"] = None
+                match en.getLinkType(link_index):
+                    case "PIPE":
+                        pass
+                    case "PUMP":
+                        controls[zone].setdefault(element, {})
+                        controls[zone][element]["speed"] = None
+                    case _:
+                        controls[zone].setdefault(element, {})
+                        controls[zone][element]["setting"] = None
 
-#         for zone, client in clients.items():
-#             pump_count = sum(
-#                 1 for element in controls[zone] if "speed" in controls[zone][element]
-#             )
-#             pump_registers = client.read_holding_registers(
-#                 1000, pump_count * 2
-#             ).registers
-#             for i, element in enumerate(
-#                 e for e in controls[zone] if "speed" in controls[zone][e]
-#             ):
-#                 converted_value = client.convert_from_registers(
-#                     pump_registers[i * 2 : i * 2 + 2], client.DATATYPE.FLOAT32
-#                 )
-#                 controls[zone][element]["speed"] = converted_value
+        for zone, client in clients.items():
+            pump_count = sum(1 for element in controls[zone] if "speed" in controls[zone][element])
+            pump_registers = client.read_holding_registers(1000, pump_count * 2).registers
 
-#             valve_count = sum(
-#                 1 for element in controls[zone] if "setting" in controls[zone][element]
-#             )
-#             valve_registers = client.read_holding_registers(
-#                 2000, valve_count * 2
-#             ).registers
-#             for i, element in enumerate(
-#                 e for e in controls[zone] if "setting" in controls[zone][e]
-#             ):
-#                 converted_value = client.convert_from_registers(
-#                     valve_registers[i * 2 : i * 2 + 2], client.DATATYPE.FLOAT32
-#                 )
-#                 controls[zone][element]["setting"] = converted_value
+            for i, element in enumerate(e for e in controls[zone] if "speed" in controls[zone][e]):
+                converted_value = client.convert_from_registers(
+                    pump_registers[i * 2 : i * 2 + 2], client.DATATYPE.FLOAT32
+                )
+                controls[zone][element]["speed"] = converted_value
 
-#         return controls
-#     except Exception as e:
-#         print(f"Error in get_controls: {e}")
-#         sys.exit(1)
+            valve_count = sum(1 for element in controls[zone] if "setting" in controls[zone][element])
+            valve_registers = client.read_holding_registers(2000, valve_count * 2).registers
+
+            for i, element in enumerate(e for e in controls[zone] if "setting" in controls[zone][e]):
+                converted_value = client.convert_from_registers(
+                    valve_registers[i * 2 : i * 2 + 2], client.DATATYPE.FLOAT32
+                )
+                controls[zone][element]["setting"] = converted_value
+
+        return controls
+    except Exception as e:
+        print(f"Error in get_controls: {e}")
+        sys.exit(1)
 
 
-# def set_controls(en: epanet, controls: dict) -> None:
-#     try:
-#         for zone, elements in controls.items():
-#             for element, control in elements.items():
-#                 link_index: int = en.getLinkIndex(f"{zone}-{element}")
-#                 if "speed" in control:
-#                     en.setLinkSettings(link_index, control["speed"])
-#                 if "setting" in control:
-#                     en.setLinkSettings(link_index, control["setting"])
+def set_controls(en: epanet, controls: dict) -> None:
+    try:
+        for zone, elements in controls.items():
+            for element, control in elements.items():
+                link_index: int = en.getLinkIndex(f"{zone}-{element}")
 
-#     except Exception as e:
-#         print(f"ERROR in set_controls: {e}")
-#         sys.exit(1)
+                if "speed" in control:
+                    en.setLinkSettings(link_index, control["speed"])
+                if "setting" in control:
+                    en.setLinkSettings(link_index, control["setting"])
+    except Exception as e:
+        print(f"ERROR in set_controls: {e}")
+        sys.exit(1)
 
 
 def read_data(en: epanet) -> dict:
@@ -147,7 +132,7 @@ def read_data(en: epanet) -> dict:
             zone, element = name_id.split("-", 1)
             data.setdefault(zone, {}).setdefault(element, {})
 
-            e: dict = data[zone][element]  # holds entry point for specific element.
+            e: dict = data[zone][element]
 
             if name_id in en.getNodeNameID():
                 node_index: int = en.getNodeIndex(name_id)
@@ -158,24 +143,12 @@ def read_data(en: epanet) -> dict:
                 e["elevation"] = str(en.getNodeElevations(node_index))
 
                 if en.getNodeType(node_index) == "TANK":
-                    e["minimum_water_level"] = str(
-                        en.getNodeTankMinimumWaterLevel(node_index)
-                    )
-                    e["maximum_water_level"] = str(
-                        en.getNodeTankMaximumWaterLevel(node_index)
-                    )
-                    e["initial_water_level"] = str(
-                        en.getNodeTankInitialLevel(node_index)
-                    )
-                    e["minimum_water_volume"] = str(
-                        en.getNodeTankMinimumWaterVolume(node_index)
-                    )
-                    e["maximum_water_volume"] = str(
-                        en.getNodeTankMaximumWaterVolume(node_index)
-                    )
-                    e["initial_water_volume"] = str(
-                        en.getNodeTankInitialWaterVolume(node_index)
-                    )
+                    e["minimum_water_level"] = str(en.getNodeTankMinimumWaterLevel(node_index))
+                    e["maximum_water_level"] = str(en.getNodeTankMaximumWaterLevel(node_index))
+                    e["initial_water_level"] = str(en.getNodeTankInitialLevel(node_index))
+                    e["minimum_water_volume"] = str(en.getNodeTankMinimumWaterVolume(node_index))
+                    e["maximum_water_volume"] = str(en.getNodeTankMaximumWaterVolume(node_index))
+                    e["initial_water_volume"] = str(en.getNodeTankInitialWaterVolume(node_index))
 
             if name_id in en.getLinkNameID():
                 link_index: int = en.getLinkIndex(name_id)
@@ -184,7 +157,7 @@ def read_data(en: epanet) -> dict:
                 e["status"] = str(en.getLinkStatus(link_index))
                 e["flow_rate"] = str(en.getLinkFlows(link_index))
 
-                match en.getLinkType(link_index):  # read values based on link type.
+                match en.getLinkType(link_index):
                     case "PIPE":
                         pass
                     case "PUMP":
@@ -192,9 +165,7 @@ def read_data(en: epanet) -> dict:
                         e["speed"] = str(en.getLinkSettings(link_index))
                         e["energy_usage"] = str(en.getLinkEnergy(link_index))
                     case _:  # default case to handle all valve types.
-                        e["setting"] = str(
-                            en.getLinkSettings(link_index)
-                        )  # i'm not sure where this is used for...
+                        e["setting"] = str(en.getLinkSettings(link_index))
 
         return data
     except Exception as e:
@@ -217,17 +188,16 @@ def write_data(clients: dict[str, ModbusTcpClient], data: dict) -> None:
 
                     client.write_registers(address, registers)
 
-                    ### TEST (LOGGING)
-                    print(
-                        f"{zone:<15} -> {element:<15} -> {k:<30}: {value:<30}, "
-                        f"registers: {str(registers):<20}, address: {address}"
-                    )
+                    # print(
+                    #     f"{zone:<15} -> {element:<15} -> {k:<30}: {value:<30}, "
+                    #     f"registers: {str(registers):<20}, address: {address}"
+                    # )
 
                 offset += len(values) * 2
 
-                print()  # blank lines for separating log entries.
-            print()
-            print()
+                # print()  # blank lines for separating log entries.
+            # print()
+            # print()
     except Exception as e:
         print(f"ERROR in write_data: {e}")
         sys.exit(1)
@@ -238,7 +208,11 @@ def main():
 
     try:
         en: epanet = setup_epanet(inp_file)
-        clients: dict[str, ModbusTcpClient] = setup_clients(get_zones(en))
+
+        zones: set[str] = get_zones(en)
+
+        clients: dict[str, ModbusTcpClient] = setup_clients(zones)
+
         en.openHydraulicAnalysis()
         en.initializeHydraulicAnalysis()
 
@@ -247,18 +221,13 @@ def main():
                 en.getTimeSimulationDuration() + en.getTimeHydraulicStep()
             )  # this way the duration is set to infinite.
 
-            # controls: dict = get_controls(clients, en)
-            # set_controls(en, controls)
+            controls: dict = get_controls(clients, en)
+            set_controls(en, controls)
 
             en.runHydraulicAnalysis()
 
             data: dict = read_data(en)
             write_data(clients, data)
-
-            ### TEST (LOGGING)
-            # import json; print(
-            # json.dumps(data, indent=4)
-            # )
 
             en.nextHydraulicAnalysisStep()
 
