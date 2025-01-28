b40d59dee7884ba230b7266285d615f92d0072e8
Vincent Hasper <125301365+haspervincent@users.noreply.github.com>
2025-01-28 17:42:05 +0100

    Update epanet.py

--- epanet/app/epanet.py
+++ epanet/app/epanet.py
@@ -42,14 +42,14 @@ def get_zones(en: epanet) -> set[str]:
 
 def setup_clients(zones: set) -> dict[str, ModbusTcpClient]:
     try:
-        # clients: dict[str, ModbusTcpClient] = {
-        #     zone: ModbusTcpClient(host=f'plc-{zone}', port=502)
-        #     for zone in zones
-        # }
         clients: dict[str, ModbusTcpClient] = {
-            zone: ModbusTcpClient(host="127.0.0.1", port=502 + i) 
-            for i, zone in enumerate(zones)
+            zone: ModbusTcpClient(host=f'plc-{zone}', port=502)
+            for zone in zones
         }
+        # clients: dict[str, ModbusTcpClient] = {
+        #     zone: ModbusTcpClient(host="127.0.0.1", port=502 + i) 
+        #     for i, zone in enumerate(zones)
+        # }
         for _, client in clients.items():
             while not client.connect():
                 time.sleep(1)
