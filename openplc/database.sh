5de70f6b3e3c1286740a1e224bba19d5d7be9179
marijnvdwekken <root@marijn-server.home>
2025-10-15 12:29:01 +0200

    Fix autostart plc zones

--- openplc/database.sh
+++ openplc/database.sh
@@ -3,8 +3,8 @@
 # https://github.com/thiagoralves/OpenPLC_v3/blob/master/webserver/openplc.db
 
 # remove all existing programs
-#SQL_REMOVE_PROGRAM="DELETE FROM Programs"
-#sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_REMOVE_PROGRAM"
+SQL_REMOVE_PROGRAM="DELETE FROM Programs"
+sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_REMOVE_PROGRAM"
 
 # add new programe as "script.st"
 SQL_PROGRAM="INSERT INTO Programs (Name, Description, File, Date_upload) VALUES ('Program Name', 'Desc', 'script.st', strftime('%s', 'now'));"
@@ -19,5 +19,5 @@ SQL_UPDATE_Modbus="UPDATE Settings SET Value = '502' WHERE Key = 'Modbus_port';"
 sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_UPDATE_Modbus"
 
 # enable openplc start run mode. Comment out if not requried.
-#SQL_Start_run_mode="UPDATE Settings SET Value = 'true' WHERE Key = 'Start_run_mode';"
-#sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_Start_run_mode"
+SQL_Start_run_mode="UPDATE Settings SET Value = 'true' WHERE Key = 'Start_run_mode';"
+sqlite3 /OpenPLC_v3/webserver/openplc.db "$SQL_Start_run_mode"
