39172870abadc7377af076b984c3c2122239f0b2
WesselMH <w.m.horsthuis@live.nl>
2025-12-15 20:16:12 +0100

    took the loops out of the read and write methods, added exceptions to except, parsed narrays to the correct types

--- epanet/app/test.py
+++ epanet/app/test.py
@@ -1,8 +1,6 @@
-import asyncio
 import decimal
 import json
 import math
-import re
 from dataclasses import dataclass
 
 from epyt import epanet
@@ -69,7 +67,7 @@ def send_inital():
         maximum_water_volume = ep.getNodeTankMaximumWaterVolume(link)
 
 
-async def get_zone_items(zone_id: str) -> tuple[list, list]:
+def get_zone_items(zone_id: str) -> tuple[list, list]:
     """fucntion to get the items from a zone
 
     Args:
@@ -91,88 +89,91 @@ async def get_zone_items(zone_id: str) -> tuple[list, list]:
     return nodes, links
 
 
-async def main():
+def get_nodedata(nodes):
+    zone_data = {}
+
+    for node in nodes:
+        node_data = dict()
+        name = ep.getNodeNameID(node)
+        node_data["type"] = ep.getNodeType(node)
+        node_data["pressure"] = round(ep.getNodePressure(node), 3)
+        node_data["quality"] = ep.getNodeActualQuality(node)
+        node_data["elevation"] = ep.getNodeElevations(node)
+        match ep.getNodeType(node):
+            case "JUNCTION":
+                node_data["demand"] = round(ep.getNodeActualDemand(node), 3)
+                node_data["head"] = ep.getNodeHydraulicHead(node)
+            case "TANK":
+                # V = pi * r^2 * h
+                node_data["level"] = round(
+                    float(
+                        ep.getNodeTankVolume(node)
+                        / (math.pi * ep.getNodeTankDiameter(node))
+                    ),
+                    3,
+                )
+            case "RESERVOIR":
+                pass
+            case _:
+                pass
+        print(name, json.dumps(node_data))
+        zone_data[name] = node_data
+    return zone_data
+
+
+def get_linkdata(links):
+    zone_data = {}
+    for link in links:
+        link_data = dict()
+        # variables of every node
+        name = ep.getLinkNameID(link)
+        link_data["type"] = ep.getLinkType(link)
+        link_data["quality"] = ep.getLinkActualQuality(link)
+        link_data["headloss"] = ep.getLinkHeadloss(link)
+        link_data["flow"] = round(float(ep.getLinkFlows(link)), 3)
+        # the same as speed = 0 or if the pipe is open of closed
+        link_data["status"] = ep.getLinkStatus(link)
+
+        match ep.getLinkType(link):
+            case "PIPE":
+                link_data["velocity"] = ep.getLinkVelocity(link)
+                link_data["length"] = ep.getLinkLength(link)
+                pass
+            case "PUMP":
+                # determent to be always 1
+                link_data["power"] = ep.getLinkPumpPower(link)
+                # used to change the rotation speed. 0 = off
+                link_data["speed"] = float(ep.getLinkSettings(link))
+                link_data["energy"] = ep.getLinkEnergy(link)
+                link_data["efficeiency"] = ep.getLinkPumpEfficiency(link)
+                link_data["state"] = ep.getLinkPumpState(link)
+            # there are more valve types but only the TCV is used
+            case "VALVE" | "TCV":
+                link_data["velocity"] = ep.getLinkVelocity(link)
+                pass
+            case _:
+                pass
+        print(name, json.dumps(link_data))
+        zone_data[name] = link_data
+    return zone_data
+
+
+def main():
     zone_ids = ["z0", "z1", "z2", "z3", "z4"]
-    data = {
-        "z0": {},
-        "z1": {},
-        "z2": {},
-        "z3": {},
-        "z4": {},
-    }
+    data = {zone: {} for zone in zone_ids}
+
     try:
         ep.openHydraulicAnalysis()
         ep.initializeHydraulicAnalysis(0)
         ep.runHydraulicAnalysis()
-        zone_tasks = [
-            asyncio.create_task(get_zone_items(zone), name=f"{zone}-items")
-            for zone in zone_ids
-        ]
-        tasks = await asyncio.gather(*zone_tasks)
-
-        # there is no way as of 11-12-2025 to get the tag value of the links and nodes
-        # epyt has a way to add comments with setNodeComment() and setLinkComment()
-        for (nodes, links), zone in zip(tasks, zone_ids):
+        for zone in zone_ids:
+            nodes, links = get_zone_items(zone)
+            # there is no way as of 11-12-2025 to get the tag value of the links and nodes
+            # epyt has a way to add comments with setNodeComment() and setLinkComment()
             print(f"Working on zone {zone}")
-            for link in links:
-                link_data = dict()
-                # variables of every node
-                name = ep.getLinkNameID(link)
-                link_data["type"] = ep.getLinkType(link)
-                link_data["quality"] = ep.getLinkActualQuality(link)
-                link_data["headloss"] = ep.getLinkHeadloss(link)
-                link_data["flow"] = round(float(ep.getLinkFlows(link)), 3)
-                # the same as speed = 0 or if the pipe is open of closed
-                link_data["status"] = ep.getLinkStatus(link)
+            data[zone].update(get_linkdata(links))
+            data[zone].update(get_nodedata(nodes))
 
-                match ep.getLinkType(link):
-                    case "PIPE":
-                        link_data["velocity"] = ep.getLinkVelocity(link)
-                        link_data["length"] = ep.getLinkLength(link)
-                        pass
-                    case "PUMP":
-                        # determent to be always 1
-                        link_data["power"] = ep.getLinkPumpPower(link)
-                        # used to change the rotation speed. 0 = off
-                        link_data["speed"] = float(ep.getLinkSettings(link))
-                        link_data["energy"] = ep.getLinkEnergy(link)
-                        link_data["efficeiency"] = ep.getLinkPumpEfficiency(link)
-                        link_data["state"] = ep.getLinkPumpState(link)
-                    # there are more valve types but only the TCV is used
-                    case "VALVE" | "TCV":
-                        link_data["velocity"] = ep.getLinkVelocity(link)
-                        pass
-                    case _:
-                        pass
-                print(name, json.dumps(link_data))
-                data[zone][name] = link_data
-
-            for node in nodes:
-                node_data = dict()
-                name = ep.getNodeNameID(node)
-                node_data["type"] = ep.getNodeType(node)
-                node_data["pressure"] = round(ep.getNodePressure(node), 3)
-                node_data["quality"] = ep.getNodeActualQuality(node)
-                node_data["elevation"] = ep.getNodeElevations(node)
-                match ep.getNodeType(node):
-                    case "JUNCTION":
-                        node_data["demand"] = round(ep.getNodeActualDemand(node), 3)
-                        node_data["head"] = ep.getNodeHydraulicHead(node)
-                    case "TANK":
-                        # V = pi * r^2 * h
-                        node_data["level"] = round(
-                            float(
-                                ep.getNodeTankVolume(node)
-                                / (math.pi * ep.getNodeTankDiameter(node))
-                            ),
-                            3,
-                        )
-                    case "RESERVOIR":
-                        pass
-                    case _:
-                        pass
-                print(name, json.dumps(node_data))
-                data[zone][name] = link_data
             print("\n")
     finally:
         json.dump(data, open("epanet/app/test.json", "w"), indent=2)
@@ -183,5 +184,4 @@ async def main():
 if __name__ == "__main__":
     # global ep
     ep = epanet(r"C:\Users\wmhor\Bureaublad\HydroSim\epanet\app\scenario.inp")
-
-    asyncio.run(main())
+    main()
